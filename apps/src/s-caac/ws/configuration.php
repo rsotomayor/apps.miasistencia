533d58c5bf5a53a7a17bf01570a29f001e8a5380
Rafael Sotomayor Brule <rsotomayor@savtec>
2015-06-30 16:24:30 -0300

    Agrega Archivos

---
+++ apps/src/s-caac/ws/configuration.php
@@ -0,0 +1,13 @@
+<?php
+
+$cscwebPath_g = '/u/savtec/public_html/cscweb/';
+
+$dbConfig_g   = array(
+  'driver'    => 'mysql',        
+  'user'      => 'savtec_user',  
+  'password'  => 'tedguro',     
+  'name'      => 'savtec_common',  
+  'host'      => 'localhost',  
+  );
+
+?>
