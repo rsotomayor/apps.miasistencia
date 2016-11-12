c690bb7f88ecead6604ce531bf76bf633103f3f1
Rafael Sotomayor Brulé <rsotomayor@savtec.cl>
2016-11-12 14:45:46 -0300

    Fix dbuser

--- apps/src/test/s-caac/ws/configuration.php
+++ apps/src/test/s-caac/ws/configuration.php
@@ -3,7 +3,7 @@
 $cscwebPath_g = '/u/savtec/public_html/cscweb/';
 
 $dbConfig_g   = array(
-  'driver'    => 'mysql',        
+  'driver'    => 'mysqli',        
   'user'      => 'db_user',  
   'password'  => 'tedguro2015',     
   'name'      => 'savtec_common',  
