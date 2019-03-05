bd196ee7e0307c992ec37fadbda8f8bc7d2dda3b
Rafael Sotomayor Brulé <rsotomayor@savtec.cl>
2019-03-05 20:22:02 -0300

    FIx app

--- apps/src/s-caac/ws/registraevento.php
+++ apps/src/s-caac/ws/registraevento.php
@@ -663,6 +663,8 @@ function publicaAcceso(&$record_p) {
   $rumbo            = str_replace (",",".", $rumbo); 
   $velocidad        = str_replace (",",".", $velocidad);  
 
+  $velocidad        = $velocidad*3.6;
+
   $szTemperature   = isset($record_p['temp'])     ? $record_p['temp'] : NULL ;
   if ( $szTemperature != NULL ) {
     sscanf($szTemperature, "%f%s", $temperature,$degree);
@@ -735,7 +737,6 @@ function publicaAcceso(&$record_p) {
 //~ Topic: savtec/sensor/gps/868789020336771
 //~ Payload[{"MSG":"GTFRI","RSP":"+RESP","ACU":1,"SPD":85.6,"AZM":123,"ALT":170.7,"LON":-71.505315,"LAT":-32.99811,"ADDRESS":"SIN DIRECCION","FECHA":"2019-02-23 17:02:20","FECHAL":"2019-02-23 14:02:20","TIME":1550941340}]
 
-    $velocidad = $velocidad*3.6;
 
     $topic    = "savtec/sensor/gps/$idmodulo";
 
